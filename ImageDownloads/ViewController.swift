c759e4582e1f37bb90a6ed835bd46469cfb8f12e
leeteukki <tklohj@naver.com>
2017-07-21 16:46:07 +0900

    First

--- ImageDownloads/ViewController.swift
+++ ImageDownloads/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var indicator: UIActivityIndicatorView!
+    
+    @IBOutlet var progressView: UIProgressView!
+    
+    
+    
+    @IBAction func downLoadAction(_ sender: Any) {
+    }
+    
+    @IBAction func stopAction(_ sender: Any) {
+    }
+  
+    @IBAction func reStartAction(_ sender: Any) {
+    }
+    
+    @IBAction func cancelAction(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
